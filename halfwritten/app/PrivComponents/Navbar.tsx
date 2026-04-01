0254be176848454928c16e782013c5ddd2b21c8f
Shashank <tiwarishashank950@gmail.com>
2026-04-01 23:20:02 +0530

    Initializing project with a navbar

---
+++ halfwritten/app/PrivComponents/Navbar.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Dock from "@/components/Dock";
+import { VscHome, VscArchive, VscAccount, VscSettingsGear } from 'react-icons/vsc';
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function Navbar() {
+  const router = useRouter();
+  const goToRoute = (path: string) => {
+    router.push(path);
+  };
+
+  const items = [
+    { icon: <VscHome size={18} />, label: 'Home', onClick: () => goToRoute('/') },
+    { icon: <VscArchive size={18} />, label: 'Archive', onClick: () => goToRoute('/archive') },
+    { icon: <VscAccount size={18} />, label: 'Profile', onClick: () => goToRoute('/profile') },
+    { icon: <VscSettingsGear size={18} />, label: 'Settings', onClick: () => goToRoute('/settings') },
+  ];
+
+  return (
+    <div>
+      <h1>Sup</h1>
+      <Dock 
+        items={items}
+        panelHeight={80}
+        baseItemSize={60}
+        magnification={80}
+      />
+    </div>
+  );
+}
